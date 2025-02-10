50e8cc20c067cefcb8170210f04e7efe39f87d4d
al <kanngo.aladdin@gmail.com>
2025-02-11 00:15:09 +0100

    Fix build errors hopefully

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,19 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  documents: "src/**/*.ts*",
   schema: "schema.graphql",
+  documents: ["src/**/*.{ts,tsx}", "!**/__tests__/**/*", "!**/*.test.{ts,tsx}"],
   overwrite: true,
   generates: {
     "src/generated/graphql.ts": {
-      plugins: [{
-        typescript: {
-          noExport: false,
+      plugins: [
+        {
+          typescript: {
+            noExport: false,
+          },
         },
-      }, "typescript-operations"],
+        "typescript-operations"
+      ],
     },
   },
 };
